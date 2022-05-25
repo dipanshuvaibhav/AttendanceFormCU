057f17d9603b62e53e0f44501993e38ecb33fc4f
Dipanshu Vaibhav <53404726+dipanshuvaibhav@users.noreply.github.com>
2022-05-25 13:41:24 +0530

    Initial Push

---
+++ YellowFormCU/app/src/main/java/com/example/yellowformcu/BlueForm.java
@@ -0,0 +1,14 @@
+package com.example.yellowformcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BlueForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_form);
+    }
+}
---
+++ YellowFormCU/app/src/main/java/com/example/yellowformcu/YellowForm.java
@@ -0,0 +1,14 @@
+package com.example.yellowformcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YellowForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yellow_form);
+    }
+}
